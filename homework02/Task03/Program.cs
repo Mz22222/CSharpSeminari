ed0a7e03b833b6294552b10e8d077e9a6a726ec5
Zara Mirzakhmedova <zara-0512@yandex.ru>
2023-12-28 20:07:23 +0300

    add homework02

---
+++ homework02/Task03/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 3: Напишите программу, которая 
+1. принимает на вход целое число из отрезка [10, 99] и 
+2. показывает наибольшую цифру числа.
+Примеры: 
+40 => 4
+96 => 9
+72 => 7
+*/
+
+Console.Write("Введите число от  10 до 99: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+//if (num < 0 ) num = num * -1;
+
+if (num >= 10 && num <= 99)
+{
+    int firstDigit = num / 10 ;
+    int secondDigit = num % 10 ;
+
+    if (firstDigit > secondDigit)
+    {
+        Console.WriteLine($"{num} => {firstDigit}");
+    }
+    else 
+    {
+        Console.WriteLine($"{num} => {secondDigit}");
+    }
+}
+else 
+{
+    Console.WriteLine("Неверное число");
+}
---
+++ homework02/Task04/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Задача 4: Напишите программу, которая на вход 
+1. принимает натуральное число N, а 
+2. на выходе показывает его цифры через запятую.
+Примеры: 
+568 => 5, 6, 8
+8 => 8
+9542 => 9, 5, 4, 2
+*/
+
+Console.Write("Введите натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 10)
+{
+    Console.Write(num);
+}
+
+else
+{
+    while (num > 0)
+    {
+        int currentDigit = num % 10;
+        num = num / 10;
+        if (num > 0)
+        {
+            Console.Write(currentDigit + ",");
+        }
+
+        else
+        {
+            Console.WriteLine(currentDigit);
+        }
+    }
+
+}
+
+//ДОДЕЛАТЬЬЬЬЬЬЬЬЬЬЬ!!!!!!!!!!!!!!
+
+
+
+
