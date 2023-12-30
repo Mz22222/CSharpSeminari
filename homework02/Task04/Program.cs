e3a76652d28c7bd9b4bc2cd89697280a8313d4fb
Zara Mirzakhmedova <zara-0512@yandex.ru>
2023-12-30 23:41:18 +0300

    update homework02

--- homework02/Task04/Program.cs
+++ homework02/Task04/Program.cs
@@ -8,6 +8,8 @@
 9542 => 9, 5, 4, 2
 */
 
+using Microsoft.VisualBasic;
+
 Console.Write("Введите натуральное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
@@ -32,11 +34,30 @@ else
             Console.WriteLine(currentDigit);
         }
     }
-
 }
 
-//ДОДЕЛАТЬЬЬЬЬЬЬЬЬЬЬ!!!!!!!!!!!!!!
+//Вариант ниже выводит всё в правильном порядке. А то, что выше, в обратном порядке.
 
+/*
+ Console.Write("Введите натуральное число: ");
+        int num = Convert.ToInt32(Console.ReadLine());
 
+        if (num < 10)
+        {
+            Console.Write(num);
+        }
+        else
+        {
+            string digitsString = "";
 
+            while (num > 0)
+            {
+                int currentDigit = num % 10;
+                num = num / 10;
+                digitsString = currentDigit + (digitsString == "" ? "" : ", " + digitsString);
+            }
+
+            Console.WriteLine(digitsString);
+        }
+*/
 
