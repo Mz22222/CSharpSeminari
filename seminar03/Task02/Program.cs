37fdf4c424411da59417d709de5e7bf7cab34796
Zara Mirzakhmedova <zara-0512@yandex.ru>
2024-01-13 17:59:06 +0300

    add Lesson3:Task02

--- seminar03/Task02/Program.cs
+++ seminar03/Task02/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задайте массив из 10 элементов, заполненный числами из промежутка [-10, 10].
+Замените отрицательные элементы на положительные, а положительные на отрицательные.
+
+Пример: 
+[1, -5, 6]
+=> [-1, 5, -6]
+-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+*/
+
+int[] array = { 4, -5, 10, 9, -6, 7, -5, -1, -10, 2, 7 };
+
+Console.Write("[");
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] *= -1;  //array[i] = array[i] * -1; 
+    Console.Write($"{array[i]} ");
+}
+
+Console.Write("]");
