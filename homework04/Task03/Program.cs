adca6e0baedef30e76b0652bf18d4040e117b814
Zara Mirzakhmedova <zara-0512@yandex.ru>
2024-01-22 18:21:33 +0300

    add homework04:Task03

---
+++ homework04/Task03/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 3: Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+*/
+
+int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+int size = array.Length;
+int count = 0;
+int index = 0;
+while (index < size / 2)
+{
+    int temp = array[index];
+    array[index] = array[size - 1 - count];
+    array[size - 1 - count] = temp;
+    count += 1;
+    index += 1;
+}
+//Console.Write(array);
+foreach (int item in array)
+{
+    Console.Write(item + " ");
+}
