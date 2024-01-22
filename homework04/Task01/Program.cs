15ff7b7863048fb9c99d66ab38fe43dfdc6818f4
Zara Mirzakhmedova <zara-0512@yandex.ru>
2024-01-22 19:01:47 +0300

    add homework04:Task01

--- homework04/Task01/Program.cs
+++ homework04/Task01/Program.cs
@@ -5,11 +5,36 @@
 
 while (true)
 {
-    Console.Write("Введите целое число: ");
-    int num = Convert.ToInt32(Console.ReadLine());
-    if (num == "q")
+    Console.Write("Введите целое число или q для выхода: ");
+    string input = Console.ReadLine();
+
+    if (input == "q")
     {
         break;
     }
+
+    int number;
+    if (int.TryParse(input, out number))
+    {
+        int sum = 0;
+        while(number >0)
+        {
+            sum += number%10;
+            number/=10;
+        }
+
+        if(sum%2 ==0)
+        {
+            Console.WriteLine("[STOP]");
+            break;
+        }
+    }
+
+    else
+    {
+        Console.WriteLine("Неккоретный ввод. Пожалуйста, введите целое число или 'q'. ");
+    }
+
 }
+
 
