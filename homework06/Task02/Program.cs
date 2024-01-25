2e1dfdc56ada9b455caa5bda932dc962a84c4db6
Zara Mirzakhmedova <zara-0512@yandex.ru>
2024-01-25 21:24:08 +0300

    add homework06:Task2

---
+++ homework06/Task02/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. 
+Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+Пример: 
+"aBcD1Ef!-" => "abcd1ef!-"
+*/
+
+void StringLower(string str)
+{
+    str = str.ToLower();
+    Console.WriteLine(str);
+}
+
+Console.WriteLine("Введите строку из латинских букв: ");
+string str = Console.ReadLine();
+
+StringLower(str);
