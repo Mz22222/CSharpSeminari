f8431e942bfc23a03b94f6c3b9a695acb16d50f6
Zara Mirzakhmedova <zara-0512@yandex.ru>
2024-01-25 19:16:28 +0300

    add seminar06:Task2

--- seminar06/Task02/Program.cs
+++ seminar06/Task02/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+На основе символов строки (тип string) сформировать массив символов (тип char[]).
+Вывести  массив на экран.
+Пример: 
+"Hello!" => ['H', 'e', 'l', 'l', 'o', '!']
+*/
+
+string st = "Hello World!";
+
+char[] resultArray = StringToCharArray(st);
+Console.Write($"\"{st}\" => ");
+
+PrintCharArray(resultArray);
+
+char[] StringToCharArray(string str)
+{
+    char[] chars = new char[st.Length];
+    for (int i = 0; i < st.Length; i++)
+    {
+        chars[i] = st[i];
+    }
+    return chars;
+}
+
+void PrintCharArray(char[] chars)
+{
+    Console.Write("[");
+    for (int i = 0; i < chars.Length; i++)
+    {
+        if (i < chars.Length - 1)
+        {
+            Console.Write($"'{chars[i]}', ");
+        }
+        else
+        {
+            Console.Write($"'{chars[i]}'");
+        }
+    }
+    Console.Write("]");
+}
