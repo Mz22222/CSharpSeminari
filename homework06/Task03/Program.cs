b50e7d25dd71f67b3b3a819fb167dbe533c6391a
Zara Mirzakhmedova <zara-0512@yandex.ru>
2024-01-25 22:17:56 +0300

    add homework06:Task3

---
+++ homework06/Task03/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+Пример: 
+"aBcD1eF!-" => Нет
+"шалаш" => да 
+"55655" => да       */
+
+Console.WriteLine("Введите произвольную строку для проверки на палиндромность: ");
+string std = Console.ReadLine();
+
+CheckPalindrome(std);
+
+void CheckPalindrome(string std)
+{
+    string revs = string.Empty;
+
+    for (int i = std.Length - 1; i >= 0; i--)
+    {
+        revs += std[i];
+    }
+
+    if (revs == std)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
