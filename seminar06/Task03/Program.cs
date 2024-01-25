2b0472f9d9ebb50ba4eaf3d358ebce4a464da565
Zara Mirzakhmedova <zara-0512@yandex.ru>
2024-01-25 19:49:46 +0300

    add seminar06:Task3

--- seminar06/Task03/Program.cs
+++ seminar06/Task03/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Считать строку с консоли, состояющую из латинских букв в нижнем регистре.
+Выяснить, сколько среди введённых букв гласных.
+
+Пример:
+"hello" => 2
+"world" => 1
+*/
+
+//using System.Runtime.Intrinsics.Arm;
+
+
+
+int CountVowels(string str)
+{
+    string stringVowels = "aeiouy";
+    int count = 0;
+    str = str.ToLower();
+
+    for (int i = 0; i < str.Length; i++)
+    {
+        for (int j = 0; j < stringVowels.Length; j++)
+        {
+            if (str[i] == stringVowels[j])
+            {
+                count++;
+            }
+
+        }
+    }
+
+    return count;
+}
+
+Console.WriteLine("Введите строку из латинских букв: ");
+string str = Console.ReadLine();
+
+int count = CountVowels(str);
+Console.WriteLine(count);
