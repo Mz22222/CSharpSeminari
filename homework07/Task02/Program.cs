8e1f9b057043de24e1d1757ece30ea88d55bc198
Zara Mirzakhmedova <zara-0512@yandex.ru>
2024-01-28 23:22:24 +0300

    update homework07:Task2

--- homework07/Task02/Program.cs
+++ homework07/Task02/Program.cs
@@ -5,5 +5,21 @@
 Использовать рекурсию, не использовать циклы.
 
 Пример: 
+[1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1
 */
+
+
+int[] array = { 1, 2, 5, 0, 10, 34 };
+ReverseArray(array, 0); 
+
+void ReverseArray(int[] Array, int i)
+{
+    if (i > Array.Length - 1) 
+    {
+        return;
+    }
+
+    ReverseArray(Array, i + 1);
+    Console.Write(Array[i] + " ");
+}
 
